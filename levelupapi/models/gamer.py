ba9b40e8c8711333b22c6205785c785a3c41c713
Dylan Morris <dylanrobertmorris@gmail.com>
2021-05-04 14:59:23 -0500

    build: deleted DB and remigrated

--- levelupapi/models/gamer.py
+++ levelupapi/models/gamer.py
@@ -5,5 +5,4 @@ class Gamer(models.Model):
 
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.CharField(max_length=100)
-    attending = models.ManyToManyField("Event", through="user_events", related_name="attendees")
     
