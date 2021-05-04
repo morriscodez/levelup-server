354e66c068711b78a0d1b395f46d6cf8c3a1ec08
Dylan Morris <dylanrobertmorris@gmail.com>
2021-05-04 15:39:04 -0500

    removed duplicate files

--- levelup.session.sql
+++ levelup.session.sql
@@ -2,4 +2,4 @@ SELECT * FROM levelupapi_game_type
 
 SELECT * FROM auth_user;
 SELECT * FROM authtoken_token;
-SELECT * FROM levelupapi_gamer;
+SELECT * FROM levelupapi_gamer
--- levelupapi/models/games.py
+++ levelupapi/models/games.py
@@ -6,7 +6,7 @@ from django.db.models.fields import CharField
 
 class Game(models.Model):
 
-    user = models.ForeignKey(User, on_delete=CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     label = CharField(max_length=50)
     game_type = models.ForeignKey("Game_Type", on_delete=models.CASCADE)
     number_of_players = models.IntegerField()
