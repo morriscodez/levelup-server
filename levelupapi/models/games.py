c844e74bfa0d66b6324d64649410c7d13229651a
Dylan Morris <dylanrobertmorris@gmail.com>
2021-05-04 15:42:50 -0500

    relaunching db

--- levelupapi/models/games.py
+++ levelupapi/models/games.py
@@ -6,7 +6,6 @@ from django.db.models.fields import CharField
 
 class Game(models.Model):
 
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
     label = CharField(max_length=50)
     game_type = models.ForeignKey("Game_Type", on_delete=models.CASCADE)
     number_of_players = models.IntegerField()
