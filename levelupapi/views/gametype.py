b86d16eedb6f5ca8715ec498447a60b96f9af4ac
Dylan Morris <dylanrobertmorris@gmail.com>
2021-05-05 14:47:16 -0500

    build: building serializer for components

---
+++ levelupapi/views/gametype.py
@@ -0,0 +1,36 @@
+#View module for handling requests about game types
+from django.http import HttpResponseServerError
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework import serializers
+from levelupapi.models import Game_Type
+
+class Game_Types(ViewSet):
+    #levelup game types
+    def retrieve(self, request, pk=None):
+        #Handle GET request for single game type
+
+        # Returns:
+        #   Response -- JSON serialized game type
+
+        try:
+            game_type = Game_Type.objects.get(pk=pk)
+            serializer = GameTypeSerializer(game_type, context={'request': request})
+            return Response(serializer.data)
+        except Exception as ex:
+            return HttpResponseServerError(ex)
+
+    def list(self, request):
+        # handle GET for all game types
+        # returns serialized list
+
+        gametypes = Game_Type.objects.all()
+        serializer = GameTypeSerializer(
+            gametypes, many=true, context={'request': request}
+        )
+        return Response(serializer.data)
+
+class GameTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Game_Type
+        fields = ('id', 'label')
