52830975d1e14d76ed04b49a0cc4132960d07959
Dylan Morris <dylanrobertmorris@gmail.com>
2021-05-07 09:32:14 -0500

    build: adding fields to event model

--- levelupapi/models/events.py
+++ levelupapi/models/events.py
@@ -4,10 +4,11 @@ from django.db.models.deletion import CASCADE
 
 class Event(models.Model):
 
+    organizer = models.ForeignKey("Gamer", on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
     description = models.TextField()
     date = models.DateTimeField()
     address = models.CharField(max_length=50)
     game = models.ForeignKey("Game", on_delete=models.CASCADE)
     attendees = models.ManyToManyField("Gamer", through="User_Event", related_name="attending")
-    
+    time = models.TimeField()
