23ff7b4aee209266d7704022188b31ef64039dd4
Dylan Morris <dylanrobertmorris@gmail.com>
2021-05-28 13:12:32 -0500

    build: created my sql query as a db view

--- levelup.session.sql
+++ levelup.session.sql
@@ -10,3 +10,20 @@ SELECT * FROM levelupapi_user_events
 UPDATE levelupapi_event
 SET name = "Drag Race Extreme"
 WHERE id = 6;
+
+CREATE VIEW GAMES_BY_USER AS
+SELECT
+    g.id,
+    g.label,
+    g.game_type_id,
+    g.number_of_players,
+    g.skill_level,
+    u.id user_id,
+    u.first_name || " " || u.last_name as full_name
+FROM 
+    levelupapi_game g
+JOIN
+    levelupapi_gamer gr ON g.gamer_id = gr.id
+JOIN 
+    auth_user u ON gr.user_id = u.id
+;
